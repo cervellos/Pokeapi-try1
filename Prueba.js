5d0c6d567ad5427f6fae50e4a086414818d8cf83
Freddy Guerra <arcobaleno262@gmail.com>
2022-06-19 23:22:56 -0300

    Hacer que la peticion espere y se muestre

--- Prueba.js
+++ Prueba.js
@@ -1,4 +1,23 @@
-const url ='Access-Control-Allow-Origin: http://pokeapi.co/api/v2/pokemon/1/';
+
+function getRandomInt(min, max) {
+    return Math.floor(Math.random()*(max - min)) + min;
+}
+
+console.log(getRandomInt(1, 151))
+
+const fetchApi = async () => {
+    try {
+        const res = await fetch('https://pokeapi.co/api/v2/pokemon/35/')
+        const data = await res.json()
+        console.log(data)
+    } catch (error){
+        console.log(error)
+    }
+}
+
+fetchApi()
+
+/*const url ='Access-Control-Allow-Origin: http://pokeapi.co/api/v2/pokemon/1/';
 const MyIp ='186.19.22.1';
-console.log(url);
+console.log(url);*/
 
